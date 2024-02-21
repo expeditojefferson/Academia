af96d0d1e1edccb17acbee65523f6c5ca9419f2f
expeditojefferson <151663580+expeditojefferson@users.noreply.github.com>
2024-02-21 16:10:27 -0300

    add customisação no card principal

---
+++ paginas/js/app.js
@@ -0,0 +1,24 @@
+// let diaDaSemana = 'Segunda'
+
+// let ElementCard = document.querySelector('.card-container')
+
+// let elementH3 = document.querySelector('#nome-treino')
+
+// let elementP = document.querySelector('#frase')
+
+// let cardImg = ''
+// let nomeTreino = ''
+// let frase = ''
+
+// switch(diaDaSemana)
+// {
+//     case 'Segunda':
+//         cardImg = "url('../assets/SiteAcademia/wallpaper.jpg')"
+//         nomeTreino = 'Full Body'
+//         frase = 'Para todas as partes do corpo'
+//         break;
+// }
+
+// ElementCard.style.backgroundImage = `${cardImg}`
+// elementH3.innerHTML = nomeTreino
+// elementP.innerHTML = frase
